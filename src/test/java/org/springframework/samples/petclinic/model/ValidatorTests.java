1e7f717de4b0f2303cdff9e12b3d5787f3fcfd97
Sandra LE THIEC <lethiec.sandra@gmail.com>
2017-12-12 12:15:07 +0100

    test unitaires erreur

--- src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
+++ src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
@@ -30,8 +30,8 @@ public class ValidatorTests {
 
         LocaleContextHolder.setLocale(Locale.ENGLISH);
         Person person = new Person();
-        person.setFirstName("");
-        person.setLastName("");
+        person.setFirstName("Smith");
+        person.setLastName("Smith");
 
         Validator validator = createValidator();
         Set<ConstraintViolation<Person>> constraintViolations = validator.validate(person);
