c21f0991bb05f53aaf00c83cfa2cf66c3e526fb6
Sandra LE THIEC <lethiec.sandra@gmail.com>
2017-12-12 12:21:19 +0100

    test unitaires1

--- src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
+++ src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
@@ -30,7 +30,7 @@ public class ValidatorTests {
 
         LocaleContextHolder.setLocale(Locale.ENGLISH);
         Person person = new Person();
-        person.setFirstName("Smith");
+        person.setFirstName("");
         person.setLastName("Smith");
 
         Validator validator = createValidator();
